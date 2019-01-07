4addfa438b80b28457c0c32d7fef904a7c808b3e
Alin <Alin@Alins-MacBook.local>
2019-01-07 13:06:29 +0200

    first init

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        String rolyText = "Long long ago, in a galaxy far, far away!";
+        int delay = 2000;
+        for (int i = 0; i < rolyText.length(); i++) {
+            long start = System.currentTimeMillis();
+            while (start >=System.currentTimeMillis() - delay) {
+
+            }
+            System.out.print(rolyText.charAt(i));
+
+        }
+
+
+    }
+}
+
